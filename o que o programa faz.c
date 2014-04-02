53aa6618f89e781e3429e0b4c554bdd58862d14b
rrafaell14 <rafaelrdsantos@hotmail.com>
2014-04-02 19:41:37 -0300

    exer lab
    exercicio sala

---
+++ o que o programa faz.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main ()
+{
+	int x; /* atribui x uma variavel inteira*/
+	scanf ("%d,&x");  /*ler a variavel x*/
+	printf ("%d,x");  /*escreve na tela a variavel x*/
+	return (0);  /*stop no programa*/
+	
+}
